dcd643e72d7da9dfa5f69d5b4e4964082acd166e
Sasuke <542711963@qq.com>
2022-07-31 17:11:49 +0800

    ok了

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
 </template>
 
 <script>
@@ -20,8 +20,9 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  /* color: #2c3e50; */
   margin-top: 60px;
   background: none;
+  color: pink;
 }
 </style>
